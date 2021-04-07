1a3013927330500a7b5e37133fef6d07df10d7d5
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-07 01:22:30 +0000

    survey.js

--- survey.js
+++ survey.js
@@ -22,14 +22,6 @@ rl.question(`What's your name? Nicknames are also acceptable :)`, (name) => {
         rl.question(`What's your favourite thing to eat for that meal?`, (favFood) => {
           rl.question(`Which sport is your absolute favourite ?`, (favSport) => {
             rl.question(`What is your superpower ? In a few words, tell us what you are amazing at!`, (superpower) => {
-              // ans.name = name;
-              // ans.activity = activity;
-              // ans.music = music;
-              // ans.meal = meal;
-              // ans.favFood = favFood;
-              // ans.favSport = favSport;
-              // ans.superpower = superpower;
-              // console.log(ans);
               process.stdout.write(
                 `\n${name} loves listening to ${music} while ${activity}, devouring ${favFood} for ${meal},
                 prefers ${favSport} over any other sport, and is amazing at ${superpower}.\n`
